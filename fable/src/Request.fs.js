046d880fd4a64fea20ea289c649576dc3030ada6
wilsonSants <pontoliteral@gmail.com>
2023-10-09 08:36:33 -0300

    fix:New fable compilation

--- fable/src/Request.fs.js
+++ fable/src/Request.fs.js
@@ -2,8 +2,8 @@ import { Record } from "../fable_modules/fable-library.4.1.4/Types.js";
 import { list_type, record_type, int32_type, string_type } from "../fable_modules/fable-library.4.1.4/Reflection.js";
 import { empty, head } from "../fable_modules/fable-library.4.1.4/List.js";
 import { SimpleJson_tryParse } from "../fable_modules/Fable.SimpleJson.3.24.0/./SimpleJson.fs.js";
-import { createTypeInfo } from "../fable_modules/Fable.SimpleJson.3.24.0/./TypeInfo.Converter.fs.js";
 import { Convert_fromJson } from "../fable_modules/Fable.SimpleJson.3.24.0/./Json.Converter.fs.js";
+import { createTypeInfo } from "../fable_modules/Fable.SimpleJson.3.24.0/./TypeInfo.Converter.fs.js";
 import { fetch$ } from "../fable_modules/Fable.Fetch.2.6.0/Fetch.fs.js";
 
 export class catReturn extends Record {
@@ -21,11 +21,10 @@ export function catReturn_$reflection() {
 }
 
 export function findList(responseList) {
-    let matchValue, inputJson, typeInfo;
-    const obj = head((matchValue = SimpleJson_tryParse(responseList), (matchValue != null) ? ((inputJson = matchValue, (typeInfo = createTypeInfo(list_type(catReturn_$reflection())), Convert_fromJson(inputJson, typeInfo)))) : (() => {
+    let matchValue;
+    return head((matchValue = SimpleJson_tryParse(responseList), (matchValue != null) ? Convert_fromJson(matchValue, createTypeInfo(list_type(catReturn_$reflection()))) : (() => {
         throw new Error("Couldn\'t parse the input JSON string because it seems to be invalid");
-    })()));
-    return obj.url;
+    })())).url;
 }
 
 export function getCatPhoto(url) {
